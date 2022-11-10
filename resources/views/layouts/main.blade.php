b89258dc54625440a3e693543147b95b6a1b1382
Alexander <alexandergaruba96@gmail.com>
2022-11-10 02:53:49 +0100

    tiny update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,7 +16,7 @@
             <div class="mx-auto max-w-7xl px-4 sm:px-6">
                 <div class="flex items-center justify-between py-6 md:justify-start md:space-x-10">
                     <div class="flex justify-start lg:w-0 lg:flex-1">
-                        <a href="#">
+                        <a href="/">
                             <span class="sr-only">IdentiME</span>
                             <img class="h-12 w-auto sm:h-12" src="{{ asset('img/identime.svg') }}" alt="IdentiME">
                         </a>
